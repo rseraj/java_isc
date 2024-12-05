3e6fad581d19bfaa6a3dbfc90548b1c0c6fba50b
Reyhaneh Seraj <63651494+rseraj@users.noreply.github.com>
2024-12-05 16:51:08 +0330

    Add files via upload

---
+++ java_isc_session1/java_isc/completeQuizeOfSession7/session7/doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"session7","l":"session7"}];updateSearchResults();
---
+++ java_isc_session1/java_isc/completeQuizeOfSession7/session7/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module session7 {
+	requires java.sql;
+}
